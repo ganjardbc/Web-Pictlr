23a667e45ff4d10500c4658e003aa7ac89cfbc3a
Ganjar Hadiatna <ganjardbc@gmail.com>
2021-07-14 09:06:06 +0700

    update api

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ window.$ = jquery
 
 // assets
 Vue.prototype.initUrl = window.location.protocol+'//'+window.location.host
-Vue.prototype.baseUrl = 'https://kebunbegonialembang.com/pictlr-api/public'
+Vue.prototype.baseUrl = 'https://pictlr.undangabi.com'
 
 Vue.prototype.urlThumbnailProfile = Vue.prototype.baseUrl+'/img/profile/thumbnails/'
 Vue.prototype.urlFotoProfile = Vue.prototype.baseUrl+'/img/profile/photos/'
